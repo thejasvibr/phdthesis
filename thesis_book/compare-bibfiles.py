27ed41233881fa30b7c22915298ab6502954d699
tbeleyur <tbeleyur@orn.mpg.de>
2021-01-17 11:44:46 +0100

    version that succesfully compiles with Ushichka chapter incroporated

---
+++ thesis_book/compare-bibfiles.py
@@ -0,0 +1,18 @@
+# module that compares bib files
+
+
+bibfiles = ['ushichka-refs.bib','intro-discussion-refs.bib']
+
+bibfile1 = bibfiles[0]
+bibfile2 = bibfiles[1]
+# code based on the example in : https://www.codegrepper.com/code-examples/python/how+to+compare+two+text+files+in+python
+
+with open(bibfile1, 'r',encoding='utf8') as file1:
+    with open(bibfile2, 'r',encoding='utf8') as file2:
+        same = set(file1).intersection(file2)
+
+same.discard('\n')
+
+with open('bibfile-commonlines.txt', 'w') as file_out:
+    for line in same:
+        file_out.write(line)
